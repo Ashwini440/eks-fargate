2697cf778c3d92b98ebf74f530050226733c389b
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-04-11 12:49:50 +0530

    Create eks-fargate-no-nodegroup-main.tf

---
+++ eks-fargate-no-nodegroup-main.tf
@@ -0,0 +1,91 @@
+# Get the default VPC
+resource "aws_eks_fargate_profile" "example" {
+  cluster_name = aws_eks_cluster.eks.name
+  fargate_profile_name = "example-fargate-profile"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution.arn
+  subnet_ids = data.aws_subnets.default.ids
+
+  selector {
+    namespace = "default"
+  }
+
+  depends_on = [aws_eks_cluster.eks]
+}
+
+# IAM Role for Fargate Pod Execution
+resource "aws_iam_role" "fargate_pod_execution" {
+  name = "eks-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_attachment" {
+  role       = aws_iam_role.fargate_pod_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = var.eks_cluster_role
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids             = data.aws_subnets.default.ids
+    endpoint_public_access = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+}
