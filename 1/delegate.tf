22dec68711f51e354aa2e1c0e110f53cf4d8f095
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-04-14 16:26:43 +0530

    Update delegate.tf

--- 1/delegate.tf
+++ 1/delegate.tf
@@ -48,56 +48,6 @@ provider "helm" {
   }
 }
 
-# Get current AWS identity
-data "aws_caller_identity" "current" {}
-
-# Get existing EKS cluster and its auth
-data "aws_eks_cluster" "eks" {
-  name = var.eks_cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = var.eks_cluster_name
-}
-
-# Provider: Kubernetes
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-# Create Namespace if not already present (optional but recommended)
-resource "kubernetes_namespace" "harness_delegate_ns" {
-  metadata {
-    name = "harness-delegate-ng"
-  }
-}
-
-# Create aws-logging configmap (Fargate expects this)
-resource "kubernetes_config_map" "aws_logging" {
-  metadata {
-    name      = "aws-logging"
-    namespace = kubernetes_namespace.harness_delegate_ns.metadata[0].name  # safer than hardcoding
-  }
-
-  data = {
-    logLevel      = "INFO"
-    logStreamName = "terraform-delegate"
-  }
-
-  depends_on = [kubernetes_namespace.harness_delegate_ns]
-}
-
-# Provider: Helm (used by Harness Delegate module)
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    token                  = data.aws_eks_cluster_auth.eks.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  }
-}
-
 module "delegate" {
   source            = "harness/harness-delegate/kubernetes"
   version           = "0.1.8"
